5ede9c0d32aa8e71770413c7ad0d290061952948
zomvr 2 <zomvroficial@gmail.com>
2025-05-19 18:51:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: `DialogContent` requires a `DialogTitle` for the component to be accessible for screen reader users.
    >
    > If you want to hide the `DialogTitle`, you can wrap it with our VisuallyHidden component.
    >
    > For more information, see https://radix-ui.com/primitives/docs/components/dialog. Error source: src/components/ui/sheet.tsx (62:5) @ _c1
    >
    >   60 |   <SheetPortal>
    >   61 |     <SheetOverlay />
    > > 62 |     <SheetPrimitive.Content
    >      |     ^
    >   63 |       ref={ref}
    >   64 |       className={cn(sheetVariants({ side }), className)}
    >   65 |       {...props}
    >
    > Call Stack
    > 27
    >
    > Show 24 ignore-listed frame(s)
    > _c1
    > src/components/ui/sheet.tsx (62:5)
    > _c1
    > src/components/ui/sheet.tsx (60:3)
    > HomePage
    > src/app/page.tsx (62:13)

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -51,12 +52,14 @@ const sheetVariants = cva(
 
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
-    VariantProps<typeof sheetVariants> {}
+    VariantProps<typeof sheetVariants> {
+  accessibleTitle?: string; // Add prop for customizable accessible title
+}
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "right", className, children, ...props }, ref) => (
+>(({ side = "right", className, children, accessibleTitle = "Menu", ...props }, ref) => (
   <SheetPortal>
     <SheetOverlay />
     <SheetPrimitive.Content
@@ -64,6 +67,9 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
+      <SheetPrimitive.Title className="sr-only">
+        {accessibleTitle}
+      </SheetPrimitive.Title>
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
